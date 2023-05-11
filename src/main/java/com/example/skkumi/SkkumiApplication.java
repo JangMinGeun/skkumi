264ed56d9c3d505535802271ab59bf4f2321f5ee
장민근 <dltns@MacBook-Pro.local>
2023-05-11 23:58:13 +0900

    init

---
+++ src/main/java/com/example/skkumi/SkkumiApplication.java
@@ -0,0 +1,13 @@
+package com.example.skkumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkkumiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkkumiApplication.class, args);
+    }
+
+}
